30142ff67a721eb5946deadefe90ab08a359cd0a
Bekian <Bekian.Cat@gmail.com>
2023-09-24 20:48:10 -0500

    added console request logging and dynamic portage

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module goByExampleServer
+module simpleGoServer
 
-go 1.21.1
+go 1.21
--- server.go
+++ server.go
@@ -2,27 +2,38 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 )
 
 func hello(w http.ResponseWriter, req *http.Request) {
-
 	fmt.Fprintf(w, "hello\n")
+	log.Printf("Received request for /hello from %s\n", req.RemoteAddr)
 }
 
 func headers(w http.ResponseWriter, req *http.Request) {
-
 	for name, headers := range req.Header {
 		for _, h := range headers {
 			fmt.Fprintf(w, "%v: %v\n", name, h)
 		}
 	}
+	log.Printf("Received request for /headers from %s\n", req.RemoteAddr)
 }
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8090" // Default port if PORT is not set
+	}
 
 	http.HandleFunc("/hello", hello)
 	http.HandleFunc("/headers", headers)
 
-	http.ListenAndServe(":8090", nil)
+	log.Printf("Server is running on port %s\n", port)
+
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		log.Fatalf("Server error: %v\n", err)
+	}
 }
